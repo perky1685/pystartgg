259bbcf13d6400c2be162db412698fd580ed05a0
s5305266 <s5305266@bournemouth.ac.uk>
2025-02-16 22:03:30 +0000

    player queries

--- pystartgg/main.py
+++ pystartgg/main.py
@@ -5,6 +5,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def MakeRequest(API_KEY, EVENT_QUERY, variables):
+    headers = {
+            'Authorization': 'Bearer ' + API_KEY,
+            'Content-Type': 'application/json'
+    }
+
+    response = requests.post(
+        "https://api.smash.gg/gql/alpha",
+        headers=headers,
+        json={'query': EVENT_QUERY, 'variables': variables}
+    )
+
+    return response.json()
+
 def GetEventResults(tourneySlug: str, videogameId: int):
     EVENT_QUERY = """query ($tourneySlug: String!, $videogameId: [ID]){
     tournament(slug: $tourneySlug){
@@ -89,8 +103,7 @@ def GetTournamentInformation(tourneySlug: str):
         id
         city
         countryCode
-        createdAt
-        endAt
+        startAt
         hasOfflineEvents
         name
         numAttendees
@@ -126,7 +139,6 @@ def GetEventsInformation(tourneySlug: str, videogameId):
             name
             numEntrants
             startAt
-            createdAt
             videogame {
                 id
                 displayName
@@ -143,22 +155,108 @@ def GetEventsInformation(tourneySlug: str, videogameId):
     API_KEY = os.getenv("API_KEY")
     return MakeRequest(API_KEY, EVENT_QUERY, variables)
 
-def MakeRequest(API_KEY, EVENT_QUERY, variables):
-    headers = {
-            'Authorization': 'Bearer ' + API_KEY,
-            'Content-Type': 'application/json'
+
+def GetPlayerInformation(slug: str):
+    #NOTE: players banner will be [0] and pfp will be [1] if a banner is set
+    #if a banner is not set, [0] will be the pfp
+    PLAYERS_QUERY = """
+        query ($slug: String!) {
+            user(slug: $slug) {
+                id
+                bio
+                birthday
+                genderPronoun
+                images{
+                    url
+                }
+                player {
+                    id
+                    prefix
+                    gamerTag
+                }
+                name
+                location {
+                    country
+                    state
+                    city
+                }
+            }
+        }
+        """
+
+    variables = {
+        "slug": slug,
     }
 
-    response = requests.post(
-        "https://api.smash.gg/gql/alpha",
-        headers=headers,
-        json={'query': EVENT_QUERY, 'variables': variables}
-    )
+    API_KEY = os.getenv("API_KEY")
+    return MakeRequest(API_KEY, PLAYERS_QUERY, variables)
 
-    return response.json()
+def GetPlayerTournaments(slug: str, page: int, videogameId):
+    PLAYERS_QUERY = """
+        query ($slug: String!, $page: Int!) {
+            user(slug: $slug) {
+                id
+                tournaments (query: {page: $page}) {
+                    nodes {
+                        id
+                        slug
+                        city
+                        countryCode
+                        startAt
+                        hasOfflineEvents
+                        name
+                        numAttendees
+                    }
+                }
+            }
+        }
+        """
+
+    variables = {
+        "slug": slug,
+        "page": page,
+    }
+
+    API_KEY = os.getenv("API_KEY")
+    return MakeRequest(API_KEY, PLAYERS_QUERY, variables)
+
+
+def GetPlayerEvents(slug: str, page: int, videogameId):
+    PLAYERS_QUERY = """
+        query ($slug: String!, $page: Int!) {
+            user(slug: $slug) {
+                id
+                tournaments (query: {page: $page}) {
+                    nodes {
+                        events{
+                            id
+                            name
+                            numEntrants
+                            startAt
+                            videogame {
+                                id
+                                displayName
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        """
+
+    variables = {
+        "slug": slug,
+        "page": page,
+    }
+
+    API_KEY = os.getenv("API_KEY")
+    return MakeRequest(API_KEY, PLAYERS_QUERY, variables)
 
 
 #print(json.dumps(GetResults("kayane-cup-arc-world-tour-2024", 33945), indent=2))
 #print(GetTournamentInformation("kayane-cup-arc-world-tour-2024"))
 #print(GetEventResults("kayane-cup-arc-world-tour-2024", 33945))
 #print(GetEventsInformation("kayane-cup-arc-world-tour-2024", [33945, 48548]))
+#print(GetPlayerInformation("c1aeaece"))
+#print(GetPlayerEvents("c1aeaece", 1, []))
+#print(GetPlayerTournaments("c1aeaece", 2))
